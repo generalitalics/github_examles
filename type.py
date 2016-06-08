14b83ad3e85d5adb70ed1b674361bbf944c80080
generalitalics <skamus00love88>
2016-06-08 15:45:16 +0300

    llll

--- type.py
+++ type.py
@@ -8,10 +8,21 @@ Crated:  08.06.2016
 # Типы данных с примерами использования
 
 # Числа
-# =====================================
-print(type(1))      # <class 'int'>
-print(type(1.0))    # <class 'float'>
-print(5/2)          # 2.5
-print(5//2)        # 2
+# ==================================================================
+print(type(1))                                  # <class 'int'>
+print(type(1.0))                                # <class 'float'>
+print(5/2)                                      # 2.5
+print(5//2)                                     # 2
+print(5**2)                                     # 25
+print(5 % 2)                                    # 1
 print()
+# ==================================================================
+
+# Списки
+# ==================================================================
+print(type([1, 2, 3, 'abc', [1, 2, 3], 4]))     # <class 'list'>
+print([c * 3 for c in 'list'])                  # list comprehension
+lst = [1, 3, 5]                                 #
+lst.append(7)                                   # append method
+print(lst)                                      # add element
 
